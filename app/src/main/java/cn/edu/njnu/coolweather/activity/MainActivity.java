98641866a2b3b231f7ab6a1f029d467f24d7d630
chensiyuan <1192031943@qq.com>
2017-04-08 20:06:50 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/njnu/coolweather/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package cn.edu.njnu.coolweather.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.edu.njnu.coolweather.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
