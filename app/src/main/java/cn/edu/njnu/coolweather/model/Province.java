ee8144af3851510147d9cea53d813b2b2b448abf
chensiyuan <1192031943@qq.com>
2017-04-08 20:51:17 +0800

    添加了几个bean和数据库操作函数

---
+++ app/src/main/java/cn/edu/njnu/coolweather/model/Province.java
@@ -0,0 +1,36 @@
+package cn.edu.njnu.coolweather.model;
+
+/**
+ * Created by Administrator on 2017/4/8.
+ */
+
+public class Province {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    private String provinceName;
+    private String provinceCode;
+}
